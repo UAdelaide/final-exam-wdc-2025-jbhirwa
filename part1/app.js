1c1df58b2628cabab5e789a67f0da9b39d9eabc4
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-21 04:43:38 +0000

    [automatic commit] 2025-06-21 04:43:38 UTC 120.20.130.62

--- part1/app.js
+++ part1/app.js
@@ -52,6 +52,4 @@ app.get('/api/walkers/summary', async (req, res) => {
     res.status(500).json({ error: 'Failed to fetch summary' });
   }
 });
-app.use(express.static(path.join(__dirname, 'public')));
-
 module.exports = app;
