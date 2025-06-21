8ffb74a0626778bb2402038d8baabc3c082af8f9
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-21 00:55:17 +0000

    [automatic commit] 2025-06-21 00:55:17 UTC ;; communications error to 208.67.222.222#53: timed out 120.20.130.62

--- part1/app.js
+++ part1/app.js
@@ -11,7 +11,11 @@ const pool = require('./db');
 app.get('/users', async (req, res) => {
   try {
     const [rows] = await pool.query('SELECT * FROM Users');
-
+    res.json(rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 app.use(logger('dev'));
 app.use(express.json());
