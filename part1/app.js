e94f611441482b75a5b6f601aeaf4067149e1fa2
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-21 01:01:53 +0000

    [automatic commit] 2025-06-21 01:01:53 UTC 120.20.130.62

--- part1/app.js
+++ part1/app.js
@@ -44,16 +44,16 @@ let db;
 
 (async () => {
   try {
-    // Connect to MySQL without specifying a database
-    const connection = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '' // Set your MySQL root password
-    });
+    // // Connect to MySQL without specifying a database
+    // const connection = await mysql.createConnection({
+    //   host: 'localhost',
+    //   user: 'root',
+    //   password: '' // Set your MySQL root password
+    // });
 
-    // Create the database if it doesn't exist
-    await connection.query('CREATE DATABASE IF NOT EXISTS testdb');
-    await connection.end();
+    // // Create the database if it doesn't exist
+    // await connection.query('CREATE DATABASE IF NOT EXISTS testdb');
+    // await connection.end();
 
     // Now connect to the created database
     db = await mysql.createConnection({
