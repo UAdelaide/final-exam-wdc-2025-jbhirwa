ee08cdfb515024a1f78fb22058568ebc0ca5364a
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-21 01:42:33 +0000

    [automatic commit] 2025-06-21 01:42:33 UTC 120.20.130.62

--- part1/app.js
+++ part1/app.js
@@ -1,32 +1,3 @@
-// var express = require('express');
-// var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
-
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-
-// var app = express();
-// const pool = require('./db');
-// app.get('/users', async (req, res) => {
-//   try {
-//     const [rows] = await pool.query('SELECT * FROM Users');
-//     res.json(rows);
-//   } catch (err) {
-//     res.status(500).json({ error: err.message });
-//   }
-// });
-
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
-// module.exports = app;
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -44,18 +15,6 @@ let db;
 
 (async () => {
   try {
-    // // Connect to MySQL without specifying a database
-    // const connection = await mysql.createConnection({
-    //   host: 'localhost',
-    //   user: 'root',
-    //   password: '' // Set your MySQL root password
-    // });
-
-    // // Create the database if it doesn't exist
-    // await connection.query('CREATE DATABASE IF NOT EXISTS testdb');
-    // await connection.end();
-
-    // Now connect to the created database
     db = await mysql.createConnection({
       host: 'localhost',
       user: 'root',
@@ -63,25 +22,6 @@ let db;
       database: 'DogWalkService'
     });
 
-    // // Create a table if it doesn't exist
-    // await db.execute(`
-    //   CREATE TABLE IF NOT EXISTS books (
-    //     id INT AUTO_INCREMENT PRIMARY KEY,
-    //     title VARCHAR(255),
-    //     author VARCHAR(255)
-    //   )
-    // `);
-
-    // // Insert data if table is empty
-    // const [rows] = await db.execute('SELECT COUNT(*) AS count FROM books');
-    // if (rows[0].count === 0) {
-    //   await db.execute(`
-    //     INSERT INTO books (title, author) VALUES
-    //     ('1984', 'George Orwell'),
-    //     ('To Kill a Mockingbird', 'Harper Lee'),
-    //     ('Brave New World', 'Aldous Huxley')
-    //   `);
-    // }
   } catch (err) {
     console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
   }
