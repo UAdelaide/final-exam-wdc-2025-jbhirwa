b48153228f70d0da5b4b28b2d6018dbf25f914e8
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-21 06:11:36 +0000

    [automatic commit] 2025-06-21 06:11:36 UTC 120.20.130.62

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -2,31 +2,31 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
 
-// GET all users (for admin/testing)
-router.get('/', async (req, res) => {
-  try {
-    const [rows] = await db.query('SELECT user_id, username, email, role FROM Users');
-    res.json(rows);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch users' });
-  }
-});
+// // GET all users (for admin/testing)
+// router.get('/', async (req, res) => {
+//   try {
+//     const [rows] = await db.query('SELECT user_id, username, email, role FROM Users');
+//     res.json(rows);
+//   } catch (error) {
+//     res.status(500).json({ error: 'Failed to fetch users' });
+//   }
+// });
 
-// POST a new user (simple signup)
-router.post('/register', async (req, res) => {
-  const { username, email, password, role } = req.body;
+// // POST a new user (simple signup)
+// router.post('/register', async (req, res) => {
+//   const { username, email, password, role } = req.body;
 
-  try {
-    const [result] = await db.query(`
-      INSERT INTO Users (username, email, password_hash, role)
-      VALUES (?, ?, ?, ?)
-    `, [username, email, password, role]);
+//   try {
+//     const [result] = await db.query(`
+//       INSERT INTO Users (username, email, password_hash, role)
+//       VALUES (?, ?, ?, ?)
+//     `, [username, email, password, role]);
 
-    res.status(201).json({ message: 'User registered', user_id: result.insertId });
-  } catch (error) {
-    res.status(500).json({ error: 'Registration failed' });
-  }
-});
+//     res.status(201).json({ message: 'User registered', user_id: result.insertId });
+//   } catch (error) {
+//     res.status(500).json({ error: 'Registration failed' });
+//   }
+// });
 
 router.get('/me', (req, res) => {
   if (!req.session.user) {
