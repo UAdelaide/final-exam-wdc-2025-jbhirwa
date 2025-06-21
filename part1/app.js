ac54ff135b1861eea55488aa24f809fb3b05a221
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-21 04:03:40 +0000

    [automatic commit] 2025-06-21 04:03:40 UTC 120.20.130.62

--- part1/app.js
+++ part1/app.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 // var cookieParser = require('cookie-parser');
 // var logger = require('morgan');
 var mysql = require('mysql2/promise');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
